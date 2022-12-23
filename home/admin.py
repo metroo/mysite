94887ec8449d92d8ed4530d31c1e2342594feff4
maddah <metroo.ri@gmail.co>
2022-12-24 02:30:11 +0330

    change grid title

--- home/admin.py
+++ home/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import Todo
 # Register your models here.
 
+@admin.display(description='نام')
+def title(self):
+    return self.title
+
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'body', 'created')
+    list_display = (title , 'body', 'created')
     search_fields = ('title', 'body')
 
 
