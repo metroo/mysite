ce6e30b0377dbc6749812690606f15adfcf18d44
maddah <metroo.ri@gmail.co>
2022-12-25 00:48:34 +0330

    EmbedContent

--- embedContent/admin.py
+++ embedContent/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
-from .models import embedContet
+from .models import EmbedContent
 # Register your models here.
 
-@admin.register(embedContet)
+@admin.register(EmbedContent)
 class EmbedContentAdmin(admin.ModelAdmin):
     list_display = ('title', 'url' , 'published')
     search_fields = ('title', 'url')
     prepopulated_fields = {'slug': ('title',)}
 
     def save_model(self, request, obj, form, change):
-        print(change)
-        content_type = ContentType.objects.get_for_model(embedContet)
+        try:
+            if(form.changed_data.index('slug')):
+                content_type = ContentType.objects.get_for_model(EmbedContent)
+                Permission.objects.filter(codename=form.initial.slug)
+        except:
+            print("not ok")
+
+
         # permission = Permission.objects.create(
         #     codename=obj.slug,
         #     name=obj.title,
--- embedContent/models.py
+++ embedContent/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib import admin
 
 # Create your models here.
-class embedContet(models.Model):
+class EmbedContent(models.Model):
 
     title = models.CharField(max_length=200)
     url = models.CharField(max_length=500)
