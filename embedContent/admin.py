2f086f7cce528fcdcbbe6fc6bc35026b3d0d82af
maddah <metroo.ri@gmail.co>
2022-12-25 20:39:46 +0330

    EmbedContentAdmin changed_data

--- embedContent/admin.py
+++ embedContent/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from .models import EmbedContent
+from django.contrib import messages
 from django.db import transaction
 
 # Register your models here.
@@ -17,14 +18,19 @@ class EmbedContentAdmin(admin.ModelAdmin):
             if 'slug' in  form.changed_data:
                 id = content_type.id
                 lastslug = form.initial
-                print(lastslug.get('slug'))
+                newslug = obj.slug
                 Permission.objects.filter(codename=lastslug.get('slug'), content_type_id=id).delete()
-
-                permission = Permission.objects.create(
-                    codename=obj.slug,
-                    name=obj.title,
-                    content_type=content_type,
-                )
+                vote_count = Permission.objects.filter(codename=newslug).count()
+                print(vote_count)
+                if(vote_count==0):
+                    permission = Permission.objects.create(
+                        codename=obj.slug,
+                        name=obj.title,
+                        content_type=content_type,
+                    )
+                else:
+                    return self.message_user(request, 'Error : Slug was not Uniq', messages.ERROR)
         except:
-            pass
+            return self.message_user(request, 'Slug was not Uniq', messages.ERROR)
+        #finally:
         super(EmbedContentAdmin, self).save_model(request, obj, form, change)
